12df1b2a796c77d1eba3ab3bf7a3c962e48920b5
Hakim47jumaev <fra9ky@gmail.com>
2024-04-23 17:09:46 +0300

    first commit

---
+++ hafta1/task1.py
@@ -0,0 +1,137 @@
+# task1
+# a=int(input())
+# b=int(input())
+
+# while b>a+1:
+#     print(b-1)
+#     b-=1
+
+
+
+#task2
+# a=int(input("Input a number to check prime or not: "))
+# if a%2==0:
+#     print("The entered number is a not prime number.")
+# else :
+#     print("The entered number is a prime number.")
+
+
+
+
+#task3
+# a=int(input("Input a number: "))
+# m=a
+# l=0
+# while a>0:
+#     k=a%10
+#     l+=k
+#     a=a//10
+# print(f"The sum of digits of {m} is:{l} ")
+
+
+
+# task4
+# n=int(input("Input the value for nth term: "))
+# l=1
+# k=0
+# while n>=l:
+#     print(f"{l}*{l}={l*l}")
+    
+#     k+=l*l
+#     l+=1
+# print(f"The sum of the above series is: {k}")
+
+
+
+
+
+
+#task5
+ 
+
+
+
+
+
+
+
+
+#task6
+# a=int(input("A="))
+# b=int(input("B="))
+# c=int(input("C="))
+# d=int(input("D="))
+
+# while b>=a:
+#     if a%d==c:
+#         print(a,end=' ')
+#     a+=1
+
+
+
+
+
+
+# task7
+# x=int(input("x="))
+# d=int(input("d="))
+# l=0
+# while x>0:
+#     k=x%10
+#     if k==d:
+#         l+=1
+#     x=x//10
+# print(l)
+
+
+
+
+
+# task8
+# x=int(input("x="))
+# l=0
+# while x>0:
+#     k=x%10
+#     l=l*10+k
+       
+#     x=x//10
+# print(f"ReverseX = {l}")
+
+
+
+
+
+
+#task9
+# a=int(input("Input number of terms: "))
+# k=0
+# l=0
+# b=0
+# print("The odd numbers are: ")
+# while a>b:
+#     if k%2!=0:
+#         print(k)
+#         b+=1
+#         l+=k
+#     k+=1
+# print(f"The Sum of odd Natural Numbers upto {a} terms: {l}")
+
+
+
+
+
+
+# task10
+# n=int(input("n="))
+# m=int(input("m="))
+# l=0
+# print(f"Numbers between {n} and {m}, divisible by 9:")
+# while m>=n:
+#     if n%9==0:
+#         print(n,end=' ')
+#         k=n
+#         l+=k
+#     n+=1
+# print(end=None)
+# print(f"The sum : {l}")  
+     
